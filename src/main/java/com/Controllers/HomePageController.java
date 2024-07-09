2d6cba4d0bd1ab66505e71dba78889db7c2e839d
Selma Halvorsen <678154@stud.hvl.no>
2024-07-09 08:20:05 +0200

    ting gjort 8/7

--- src/main/java/com/Controllers/HomePageController.java
+++ src/main/java/com/Controllers/HomePageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomePageController {
 
-        @GetMapping("/")
-        public String test() {
-            return "App is running";
-        }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
 }
